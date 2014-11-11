ae82c60822d5aa573b86a39be18055b9f434667c
maro <maro@maro-pawel>
2014-11-11 23:48:35 +0100

    correction of indentation

--- plugins/EvenUnevenTimes/API.php
+++ plugins/EvenUnevenTimes/API.php
@@ -30,40 +30,35 @@ class API extends \Piwik\Plugin\API
      */
     public function getEvenUnevenTimesReport($idSite, $period, $date, $segment = false) 
     {
+        // get data in relation to visit hour (local time)
+        $data = \Piwik\Plugins\VisitTime\API::getInstance()->getVisitInformationPerServerTime(
+            $idSite, $period, $date, $segment
+        );   
+        $data->applyQueuedFilters();
 
-    // get data in relation to visit hour (local time)
-    $data = \Piwik\Plugins\VisitTime\API::getInstance()->getVisitInformationPerServerTime(
-        $idSite, $period, $date, $segment
-    );   
-    $data->applyQueuedFilters();
+        // we could create a new instance by using new DataTable(),
+        // but we would loose DataTable metadata, which can be useful.        
+        $result = $data->getEmptyClone($keepFilters = false);      
+        $rows=array('Even'=>0, 'Uneven'=>0);
 
-    // we could create a new instance by using new DataTable(),
-    // but we would loose DataTable metadata, which can be useful.        
-    $result = $data->getEmptyClone($keepFilters = false);      
-    $rows=array('Even'=>0, 'Uneven'=>0);
-
-    // increment visits counter for even end uneven hour of visit
-    foreach ($data->getRows() as $visitRow) {
-        $nb_visits = $visitRow->getColumn('nb_visits');
-        if((int)$visitRow->getColumn('label') % 2 == 0) {
-            $rows['Even'] += $nb_visits;
-        } else {
-            $rows['Uneven'] += $nb_visits;
+        // increment visits counter for even end uneven hour of visit
+        foreach ($data->getRows() as $visitRow) {
+            $nb_visits = $visitRow->getColumn('nb_visits');
+            if((int)$visitRow->getColumn('label') % 2 == 0) {
+                $rows['Even'] += $nb_visits;
+            } else {
+                $rows['Uneven'] += $nb_visits;
+            }
         }
-    }
 
-    
-    // insert data from array with counters to return object (DataTable)
-    foreach($rows as $label=>$nb_visits) {
-        $result->addRowFromSimpleArray(array(
-            'label'     => Piwik::translate("EvenUnevenTimes_{$label}"),
-            'nb_visits' => $nb_visits
-        ));            
+        // insert data from array with counters to return object (DataTable)
+        foreach($rows as $label=>$nb_visits) {
+            $result->addRowFromSimpleArray(array(
+                'label'     => Piwik::translate("EvenUnevenTimes_{$label}"),
+                'nb_visits' => $nb_visits
+            ));            
+        }
+        return $result;                     
     }
-    return $result;                  
-    
-    }
-    
-    
-    
+ 
 }
