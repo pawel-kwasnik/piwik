61e5f51ee24a5ae75708042382f04dd83d785504
maro <maro@maro-pawel>
2014-11-13 20:47:56 +0100

    move getEvenUnevenTimesReport logic to Archiver

--- plugins/EvenUnevenTimes/API.php
+++ plugins/EvenUnevenTimes/API.php
@@ -34,7 +34,6 @@ class API extends \Piwik\Plugin\API
         Piwik::checkUserHasViewAccess($idSite);
         $archive = Archive::build($idSite, $period, $date, $segment);   
         $dataTable = $archive->getDataTable($name);
-        $dataTable->queueFilter('ReplaceColumnNames');
         return $dataTable;
     }    
     
@@ -49,41 +48,8 @@ class API extends \Piwik\Plugin\API
      */
     public function getEvenUnevenTimesReport($idSite, $period, $date, $segment = false) 
     {
-
-
-        // get data in relation to visit hour - using VisitTime plugin
-//        $data = \Piwik\Plugins\VisitTime\API::getInstance()->getVisitInformationPerServerTime(
-//            $idSite, $period, $date, $segment
-//        );   
-
-        // get data in relation to visit hour using own archiver
         // use 'EvenUnevenTimes_utcTime' or 'EvenUnevenTimes_visitorTime'
-        $data = $this->getDataTable('EvenUnevenTimes_utcTime', $idSite, $period, $date, $segment);
-        $data->applyQueuedFilters();
-        
-        // we could create a new instance by using new DataTable(),
-        // but we would loose DataTable metadata, which can be useful.        
-        $result = $data->getEmptyClone($keepFilters = false);      
-        $rows=array('Even'=>0, 'Uneven'=>0);
-
-        // increment visits counter for even end uneven hour of visit
-        foreach ($data->getRows() as $visitRow) {
-            $nb_visits = $visitRow->getColumn('nb_visits');
-            if((int)$visitRow->getColumn('label') % 2 == 0) {
-                $rows['Even'] += $nb_visits;
-            } else {
-                $rows['Uneven'] += $nb_visits;
-            }
-        }
-        
-        // insert data from array with counters to return object (DataTable)
-        foreach($rows as $label=>$nb_visits) {
-            $result->addRowFromSimpleArray(array(
-                'label'     => Piwik::translate("EvenUnevenTimes_{$label}"),
-                'nb_visits' => $nb_visits
-            ));            
-        }
-        return $result;                     
+        return $this->getDataTable('EvenUnevenTimes_utcTime', $idSite, $period, $date, $segment);                     
     }
  
 }
--- plugins/EvenUnevenTimes/Archiver.php
+++ plugins/EvenUnevenTimes/Archiver.php
@@ -8,23 +8,24 @@
  */
 
 namespace Piwik\Plugins\EvenUnevenTimes;
-use Piwik\Metrics;
-use Piwik\DataArray;
-use Piwik\Date;
+use Piwik\Piwik;
+use Piwik\DataTable;
+//use Piwik\Metrics;
+//use Piwik\DataArray;
+//use Piwik\Date;
+//use Piwik\DataTable\Row;
+
 
 class Archiver extends \Piwik\Plugin\Archiver
 {
     const UTC_TIME_RECORD_NAME = 'EvenUnevenTimes_utcTime';
     const VISITOR_TIME_RECORD_NAME = 'EvenUnevenTimes_visitorTime';
     
-    const UTC_TIME_RECORD_FIELD = 'HOUR(log_visit.visit_last_action_time)';
-    const VISITOR_TIME_RECORD_FIELD = 'HOUR(log_visit.visitor_localtime)';
-    
 
     public function aggregateDayReport()
     {
-        $this->aggregateByLabel(self::UTC_TIME_RECORD_FIELD, self::UTC_TIME_RECORD_NAME);
-        $this->aggregateByLabel(self::VISITOR_TIME_RECORD_FIELD, self::VISITOR_TIME_RECORD_NAME);   
+        $this->aggregateByLabel('HOUR(log_visit.visit_last_action_time)', self::UTC_TIME_RECORD_NAME);
+        $this->aggregateByLabel('HOUR(log_visit.visitor_localtime)', self::VISITOR_TIME_RECORD_NAME);   
     }
     
     public function aggregateMultipleReports()
@@ -39,9 +40,25 @@ class Archiver extends \Piwik\Plugin\Archiver
     }
     
     private function aggregateByLabel($labelSQL, $recordName)
-    {
-        $report = $this->getLogAggregator()->getMetricsFromVisitByDimension($labelSQL)->asDataTable()->getSerialized();
-        $this->getProcessor()->insertBlobRecord($recordName, $report);
+    {        
+        $data = $this->getLogAggregator()->getMetricsFromVisitByDimension($labelSQL)->asDataTable();
+        $data->filter('ReplaceColumnNames');
+        
+        // prepare simple array
+        $parityArray=array( 
+            array( 'label' => Piwik::translate("EvenUnevenTimes_Uneven"), 'nb_visits' => 0), 
+            array( 'label' => Piwik::translate("EvenUnevenTimes_Even"), 'nb_visits' => 0), 
+        );
+        
+        // increment visits counter for even end uneven hour of visit
+        foreach ($data->getRows() as $visitRow) {
+            $parity=((int)$visitRow->getColumn('label') % 2 == 0);
+            $parityArray[$parity]['nb_visits'] += $visitRow->getColumn('nb_visits');
+        }
+        
+        $dataTable = new DataTable();
+        $dataTable->addRowsFromSimpleArray($parityArray);      
+        $this->getProcessor()->insertBlobRecord($recordName, $dataTable->getSerialized());
     }    
 
 }
